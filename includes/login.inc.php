a6aff08618711c11c84986ed087f6e83f4690224
yi-ye-zhi-qiu <61097920+yi-ye-zhi-qiu@users.noreply.github.com>
2020-11-16 12:28:35 -0800

    Create login.inc.php

---
+++ includes/login.inc.php
@@ -0,0 +1,71 @@
+<?php
+if (isset($_POST['login-submit'])) {
+
+  require 'dbh.inc.php';
+
+  $mailuid = $_POST['mailuid'];
+  $password = $_POST['pwd'];
+  
+  //check empty inputs?
+  if(empty($mailuid) && empty($password)) {
+    header("Location: ../index.php?error=emptyuidpass&un=".$mailuid);
+    exit();
+  }
+  if (empty($mailuid)) {
+    header("Location: ../index.php?error=emptyuidpass&un=".$mailuid);
+    exit();
+  }
+  if (empty($password)) {
+    header("Location: ../index.php?error=emptypass&un=".$mailuid);
+    exit();
+  }
+
+  else {
+
+
+
+    $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../index.php?error=sqlerror");
+      exit();
+    }
+    else {
+
+      // If there is no error then we continue the script!
+
+      mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      if ($row = mysqli_fetch_assoc($result)) {
+        $pwdCheck = password_verify($password, $row['pwdUsers']);
+        if ($pwdCheck == false) {
+          header("Location: ../index.php?error=wronguidpwd");
+          exit();
+        }
+        else if ($pwdCheck == true) {
+
+
+          session_start();
+          $_SESSION['id'] = $row['idUsers'];
+          $_SESSION['uid'] = $row['uidUsers'];
+          $_SESSION['email'] = $row['emailUsers'];
+          // Now the user is registered as logged in and we can now take them anywhere
+          header("Location: ../signup.php?login=success");
+          exit();
+        }
+      }
+      else {
+        header("Location: ../index.php?error=wronguidpwd");
+        exit();
+      }
+    }
+  }
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+}
+else {
+  //invalid page url access redirects immediately
+  header("Location: ../index.php");
+  exit();
+}
